ca19eee75e5fe9c84c2c22c9374e556beddd3ded
danielskruks <danielkruk79@gmail.com>
2023-12-31 19:04:08 +0000

    Renaming the pages with emojis and proper naming

---
+++ 1_🏎️_Homepage.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='AiTHLETES F1 Hub',
+    page_icon="🏎️"
+)
+
+st.title("Welcome to your new Formula One Hub!")
+st.text("adding some text")
+st.sidebar.success("Select one of the above pages to navigate through our Website")
---
+++ pages/2_📊_Data_Hub.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='AiTHLETES F1 Data Hub',
+    page_icon="📊"
+)
+
+st.title("Data Hub")
+st.text("In this page you will be able to find all things data related about the ongoing F1 Season!")
+
