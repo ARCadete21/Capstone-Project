69cb8824ccca66495dde8d7e971b669b8288f53b
danielskruks <danielkruk79@gmail.com>
2023-12-31 18:36:19 +0000

    testing some simple streamlit to check if I know stuff

---
+++ mt.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Title of the page',
+    page_icon=":slight_smile:"
+)
+
+st.title("Main Page")
+st.sidebar.success("Select one of the below pages")
