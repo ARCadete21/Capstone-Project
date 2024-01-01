b671e4a964213697c2fb34daeb0435f88a099fb1
danielskruks <danielkruk79@gmail.com>
2024-01-01 00:49:43 +0000

    Added new rows and tested some stuff that could be useful

--- pages/2_📊_Data_Hub.py
+++ pages/2_📊_Data_Hub.py
@@ -1,6 +1,12 @@
+#Importing the required packages
 import streamlit as st
+import pandas as pd
+
+#Used to create some of the plots
+import plost as pls
 
 st.set_page_config(
+    layout='wide',
     page_title='AiTHLETES F1 Data Hub',
     page_icon="📊"
 )
@@ -8,3 +14,14 @@ st.set_page_config(
 st.title("Data Hub")
 st.text("In this page you will be able to find all things data related about the ongoing F1 Season!")
 
+####
+# with open('style.css') as f:
+#     st.markdown(f'<style>{f.read()}</style>')
+
+st.sidebar.subheader("testing some tests")
+
+
+
+#Importing the datasets from our github repo
+# drivers_data = pd.read_csv('data/drivers.csv')
+# drivers_data
---
+++ pages/3_🤖_F1_Pilot.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    layout='wide',
+    page_title='AiTHLETES F1 Pilot',
+    page_icon="🏎️"
+)
+
+st.text("Insert Bot :)")
