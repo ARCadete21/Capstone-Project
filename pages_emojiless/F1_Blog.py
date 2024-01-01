17c6f5fc7210319d3203f2b9d581605b956688d8
danielskruks <danielkruk79@gmail.com>
2024-01-01 01:54:42 +0000

    testing a new menu, which required a whole new folder (not likely to use)

---
+++ pages_emojiless/F1_Blog.py
@@ -0,0 +1,19 @@
+# import streamlit as st
+
+
+# st.set_page_config(
+#     layout='wide',
+#     page_title='AiTHLETES F1 Hub | F1 Blog',
+#     page_icon="📢",
+#     initial_sidebar_state='collapsed'
+# )
+
+import streamlit as st
+
+def show_f1_blog():
+    st.title("F1 Data Hub - F1 Blog")
+    # Add content specific to the F1 Blog page
+
+
+
+
